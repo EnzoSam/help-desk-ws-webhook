0e2ae9596dd715c57092da4ab110acab6d8d2a26
Enzo Samudio <enzosamudio@gmail.com>
2025-11-29 10:26:54 -0300

    add test ednpoint

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.get('/webhook', (req, res) => {
+    res.status(200).send('OK!!!');
+});
+
 app.post('/webhook', (req, res) => {
     console.log('Received webhook:', req.body);
     res.status(200).send('Webhook received');
